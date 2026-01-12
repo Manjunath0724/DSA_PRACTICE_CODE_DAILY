f517151f6a975951287d422797230bd3ac997f9b
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-01-12 22:55:25 +0530

    Daily dsa

---
+++ Chapter16/Shortest.java
@@ -0,0 +1,28 @@
+package Chapter16;
+
+public class Shortest {
+    public static float getShortestPath(String path) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            if (dir == 'N') {
+                y++;
+            } else if (dir == 'S') {
+                y--;
+            } else if (dir == 'E') {
+                x++;
+            } else if (dir == 'W') {
+                x--;
+            }
+        }
+        int X2 = x * x;
+        int Y2 = y * y;
+        return (float) Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String args[]) {
+        String path = "WNEEN";
+        System.out.println(getShortestPath(path));
+    }
+}
