990fc50fc3b069f6328fa971bae3c31c799608af
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-02-09 22:04:30 +0530

    Daily DSA

---
+++ leetcode/lt_142.java
@@ -0,0 +1,46 @@
+package leetcode;
+//Floyd’s cycle detection algorithm
+public class lt_142 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        lt_142 sol = new lt_142();
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next; // Create a cycle
+
+        ListNode cycleStart = sol.detectCycle(head);
+        if (cycleStart != null) {
+            System.out.println("Cycle starts at node with value: " + cycleStart.val);
+        } else {
+            System.out.println("No cycle detected.");
+        }
+    }
+}
