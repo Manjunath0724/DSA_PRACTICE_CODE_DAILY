faae98be8005a0f8a471166d9942494e079312a6
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-02-11 23:09:02 +0530

    Daily DSA

---
+++ leetcode/lt_234.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+public class lt_234 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null, curr = slow;
+        while (curr != null) {
+
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+
+        }
+        ListNode first = head, second = prev;
+        while (second != null) {
+            if (first.val != second.val)
+                return false;
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        lt_234 sol = new lt_234();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+        head.next.next.next.next = new ListNode(1);
+        boolean result = sol.isPalindrome(head);
+        System.out.println("Is the linked list a palindrome? " + result);
+    }
+}
