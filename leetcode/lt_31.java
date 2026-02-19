05ad38cef54156abd230f27b17b47941bc157bf0
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-02-19 21:43:18 +0530

    Daily DSA(Added Some Information for understanding of the procedure)

--- leetcode/lt_31.java
+++ leetcode/lt_31.java
@@ -4,18 +4,20 @@ public class lt_31 {
     public void nextPermutation(int[] nums) {
         int ind1 = -1;
         int ind2 = -1;
-
+        // step 1 find breaking point
         for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 ind1 = i;
                 break;
             }
         }
-
+        // if there is no breaking point
         if (ind1 == -1) {
             reverse(nums, 0);
+        }
 
-        } else {
+        else {
+            // step 2 find next greater element and swap with ind2
             for (int i = nums.length - 1; i >= 0; i--) {
                 if (nums[i] > nums[ind1]) {
                     ind2 = i;
@@ -24,6 +26,7 @@ public class lt_31 {
             }
 
             swap(nums, ind1, ind2);
+            // step 3 reverse the rest right half
             reverse(nums, ind1 + 1);
         }
     }
