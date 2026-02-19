997b180799e1918d9968d8eaefe544b6d6b21c6e
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-02-19 12:25:00 +0530

    DSA learning((Divide and Conquer problems )->Merge sort

---
+++ Chapter20/merge.java
@@ -0,0 +1,61 @@
+
+package Chapter20;
+
+public class merge {
+    public static void mergesort(int[] arr, int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2;
+        mergesort(arr, si, mid);// left
+        mergesort(arr, mid + 1, ei);// right
+        merge(arr, si, mid, ei);
+
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+                // k++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+                // k++;
+            }
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+
+        }
+
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 6, 3, 9, 5, 2, 8 };
+        mergesort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
