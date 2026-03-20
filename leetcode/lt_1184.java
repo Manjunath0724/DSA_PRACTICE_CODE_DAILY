3c8d19f7ed3d0970dff1b614704d4b4929770fce
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-03-20 23:03:45 +0530

    Daily DSA(pb-1184)

---
+++ leetcode/lt_1184.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class lt_1184 {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int front = 0;
+        int back = 0;
+        for (int i = start; i != destiination; i = (1 + 1) % distance.length) {
+            front += distance[i];
+        }
+        for (int i = destination; i != start; i = (1 + 1) % distance.length) {
+            back += distance[i];
+        }
+        return Math.min(front, back);
+    }
+
+    public static void main(String[] args) {
+        lt_1184 sol = new lt_1184();
+        int[] distance = { 1, 2, 3, 4 };
+        int start = 0;
+        int destination = 2;
+        int result = sol.distanceBetweenBusStops(distance, start, destination);
+        System.out.println(result);
+    }
+}
