5fe717369a7e64a593095e984bce8ffad6903b2c
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-04-22 20:47:49 +0530

    Daily DSA(pb-2656)

---
+++ leetcode/lt_2656.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class lt_2656 {
+
+    public int maximizeSum(int[] nums, int k) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+        int largest = nums[nums.length - 1];
+        for (int i = 0; i < k; i++) {
+            maxSum += largest;
+            largest++;
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        lt_2656 s = new lt_2656();
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int k = 3;
+        int result = s.maximizeSum(nums, k);
+        System.out.println(result); // Output: 18
+    }
+}
