cdb3dabcb4c789a03663c3416f5343226299df67
Manjunath0724 <manjunathgavandi8161@gmail.com>
2026-04-23 20:52:40 +0530

    Daily DSA(pb-2639)

---
+++ leetcode/lt_2639.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class lt_2639 {
+    public int[] findColumnWidth(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int ans[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < m; j++) {
+                max = Math.max(max, String.valueOf(grid[j][i]).length());
+            }
+            ans[i] = max;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        lt_2639 s = new lt_2639();
+        int[][] grid = { { 1, 22, 333 }, { 4444, 55555, 666666 } };
+        int[] result = s.findColumnWidth(grid);
+        for (int width : result) {
+            System.out.print(width + " "); // Output: 4 5 6
+        }
+    }
+}
